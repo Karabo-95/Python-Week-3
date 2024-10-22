936c63f06d8d44fc3d4776686abaddb3db61fb66
Karabo Molokwane <136061685+Karabo-95@users.noreply.github.com>
2024-10-22 21:03:40 +0200

    Delete User_input.py

--- User_input.py
+++
@@ -1,6 +0,0 @@
-# Ask the user for their name, age, and location
-name = input("Karabo Molokwane: ")
-age = input("29: ")
-location = input("South_Africa: ")
-# Print out a personalized message
-print(f"Hello {name}, you are {age} years old and live in {location}.")
